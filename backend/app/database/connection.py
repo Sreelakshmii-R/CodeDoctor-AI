e1e0a8c77d72370699cdd047833350516823e407
Sreelakshmi <sreelakshmiramesh296@gmail.com>
2026-07-30 13:22:59 +0530

    Configure PostgreSQL public schema

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -5,9 +5,11 @@ from app.core.config import settings
 
 
 engine = create_engine(
-    settings.database_url
+    settings.database_url,
+    connect_args={
+        "options": "-csearch_path=public"
+    }
 )
-
 
 SessionLocal = sessionmaker(
     autocommit=False,
